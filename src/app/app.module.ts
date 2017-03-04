0349e7567d78af4468a2261d4f8531fa7009e385
Henrique <hs.ccti@gmail.com>
2017-03-04 09:34:21 -0300

    Iniciando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,37 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//Rotas
+import {Routing, RoutingProviders } from './app.routing';
+
+// ROOT
 import { AppComponent } from './app.component';
+
+// SHARED
+import { HeadbarComponent } from './components/shared/headbar/headbar.component';
+import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+// COMPONENTS
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+// PAGES
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadbarComponent,
+    SubMenuComponent,
+    FooterComponent,
+    ProductListComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    CartPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
